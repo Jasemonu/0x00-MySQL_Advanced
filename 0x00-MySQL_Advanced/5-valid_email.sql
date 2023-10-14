c996bd330881f655a068d2337cda5c01c4a9e1c5
Jasemonu <jasemonu@gmail.com>
2023-10-14 05:46:05 -0700

    5-valid_email.sql

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- a SQL script that creates a trigger that resets the attribute
+-- valid_email only when the email has been changed
+
+DELIMETER //
+CREATE TRIGGER valid_email
+BEFORE UPDATE ON user
+FOR EACH ROW
+BEGIN
+	IF NEW.email <> OLD.email THEN
+		SET NEW.valid_email = 0;
+	END IF;
+END;
+//
+DELIMETER ;
