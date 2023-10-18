b3cc80fc7850d9c420d0209b78fb98f0364347fb
Jasemonu <jasemonu@gmail.com>
2023-10-18 22:08:09 +0000

    8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -4,15 +4,12 @@
 import pymongo
 
 
-def list_all(mongo_collection):
-    # Initialize an empty list to store the documents
-    document_list = []
-    if mongo_collection is None:
-        return []
-    # Iterate through the documents in the collection
-    for document in mongo_collection.find():
-        document_list.append(document)
-    
-    # Return the list of documents
-    return document_list
+import pymongo
 
+
+def list_all(mongo_collection):
+    """Return list of all docs in collection"""
+    if not mongo_collection:
+        return []
+    docs = mongo_collection.find()
+    return [doc for doc in docs]
