394905d5b096fffe6848feeb7548bb050c3af613
Jasemonu <jasemonu@gmail.com>
2023-10-18 22:32:42 +0000

    8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Write a Python function that lists all documents in a collection:"""
 
+
 import pymongo
 
 
 def list_all(mongo_collection):
-    if mongo_collection is None:
+    if not mongo_collection:
         return []
     document_list = []
     documents = mongo_collection.find()
