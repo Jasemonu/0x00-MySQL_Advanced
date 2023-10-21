518e15f6242c039200c7e7893985ce61b74c93db
Jasemonu <jasemonu@gmail.com>
2023-10-21 10:27:37 +0000

    adding two files

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -4,9 +4,10 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient("mongodb://localhost:27017")
-    db = client["logs"]
-    col = db["nginx"]
+    db = client.logs
+    col = db.nginx
 
+    # getting the methods and count for each
     logs_count = col.count_documents({})
 
     print(f"{logs_count} logs\nMethods:")
@@ -20,4 +21,27 @@ if __name__ == "__main__":
     get_status_count = col.count_documents({'method': 'GET',
                                             'path': '/status'})
 
-    print(f"{get_status_count} status check")
+    print(f"{get_status_count} status check\nIPs:")
+
+    # adding the top 10 of the most present IPs
+    group_stage = {
+            "$group": {
+                "_id": "$ip",
+                "count": {"$sum": 1}
+                }
+            }
+
+    sort_stage = {
+            "$sort": {"count": -1}
+            }
+
+    limit_stage = {
+            "$limit": 10
+            }
+
+    pipeline = [group_stage, sort_stage, limit_stage]
+
+    result = col.aggregate(pipeline)
+
+    for row in result:
+        print(f"\t{row['_id']}: {row['count']}")
